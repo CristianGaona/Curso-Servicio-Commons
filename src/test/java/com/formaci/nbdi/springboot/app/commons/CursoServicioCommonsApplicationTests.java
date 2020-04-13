404290ccd289e0935d6250a2814161973ca3e576
CristianGaona <crgaonas24@gmail.com>
2020-04-13 17:57:10 -0500

    Creando librería commons y generando jar usando maven

---
+++ src/test/java/com/formaci/nbdi/springboot/app/commons/CursoServicioCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaci.nbdi.springboot.app.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoServicioCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
